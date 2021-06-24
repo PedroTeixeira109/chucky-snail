233c3968a66245739c8cd84c3d63a86e3e2d4e03
PedroTeixeira109 <pedroteixeira.7@hotmail.com>
2021-06-24 11:47:56 +0100

    updte

---
+++ js/game.js
@@ -0,0 +1,22 @@
+import {LoadScene} from './loadcene.js';
+import {Level001} from './level001.js'
+const config = {
+    width: 2048,
+    height: 2048,
+    type: Phaser.AUTO,
+    parent: 'game-canvas',
+    backgroundColor: '#e7fdff',
+    scene: [LoadScene, Level001],
+    physics: {
+        default: 'arcade',
+        arcade: {
+               gravity: {
+                   y: 800
+               },
+               debug: true
+        }
+    },
+    pixelArt: true
+}
+
+new Phaser.Game(config);
---
+++ js/loadscene.js
@@ -0,0 +1,17 @@
+export class LoadScene extends Phaser.Scene 
+{
+    constructor() 
+    {
+        super('LoadScene');
+    }
+
+    preload() 
+    {
+        this.load.image('background', './images/blue_land.png');
+    }
+
+    create() 
+    {
+        this.scene.start('Level001');
+    }
+}
